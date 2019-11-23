085f99df1c4460121eb1e481daee566af5fe33cd
Gregory Green <ggreen@pivotal.io>
2019-11-22 21:18:16 -0500

    Moved from pivotal REPO

---
+++ src/main/java/io/pivotal/services/dataTx/geode/serialization/PDX.java
@@ -0,0 +1,144 @@
+package io.pivotal.services.dataTx.geode.serialization;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.geode.pdx.JSONFormatter;
+import org.apache.geode.pdx.JSONFormatterException;
+import org.apache.geode.pdx.PdxInstance;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * @author greg green
+ * PDX conversion utility
+ */
+public class PDX
+{
+	/**
+	 * JSON_TYPE_ATTRIBUTE = "@type"
+	 */
+	public static final String JSON_TYPE_ATTRIBUTE = "@type";
+
+	public static String toJsonFromNondPdxObject(Object obj)
+	{
+		return new ToJsonFromNonPdxObject().convert(obj);
+	}
+
+	public static String toJSON(PdxInstance pdxInstance, String className)
+	{
+
+		String json = JSONFormatter.toJSON(pdxInstance);
+
+		return addTypeToJson(json, className);
+
+	}//-------------------------------------------
+	static String addTypeToJson(String json, String type)
+	{
+		if(json.contains(JSON_TYPE_ATTRIBUTE))
+			return json;
+
+		String prefix = new StringBuilder().append("{\"")
+				.append(PDX.JSON_TYPE_ATTRIBUTE)
+				.append("\":\"")
+				.append(type)
+				.append("\", ").toString();
+
+		json = json.replaceFirst("\\{",prefix);
+		return json;
+	}//-------------------------------------------
+	public static PdxInstance fromJSON(String json)
+	{
+		try{
+
+
+
+			 if(json == null || json.length() == 0)
+			 	throw new IllegalArgumentException("json required");
+
+			validateJson(json);
+
+			return JSONFormatter.fromJSON(json);
+		}
+		catch(JSONFormatterException e){
+
+			String message = e.getMessage();
+
+			if(e.getCause() != null)
+				message = message+" cause:"+e.getCause().getMessage();
+
+			message += " json:"+json;
+
+			throw new IllegalArgumentException(message);
+		}
+	}//-------------------------------------------
+
+	public static void validateJson(String json)
+	{
+		if(!json.contains(JSON_TYPE_ATTRIBUTE))
+			throw new IllegalArgumentException("Expected JSON to contain attribute:" + JSON_TYPE_ATTRIBUTE);
+
+		try
+		{
+			new ObjectMapper().readTree(json);
+		}
+		catch(IOException e)
+		{
+			throw new IllegalArgumentException("Invalid json:"+json+" ERROR:"+e);
+		}
+
+
+	}
+
+	public static SerializationPdxEntryWrapper toSerializePdxEntryWrapperFromJson(String json)
+	{
+		try
+		{
+			ObjectMapper om = new ObjectMapper();
+			SerializationPdxEntryWrapper wrapper = om.readValue(json,SerializationPdxEntryWrapper.class);
+			return wrapper;
+
+		}
+		catch(IOException e)
+		{
+			throw new RuntimeException(e);
+		}
+
+
+	}//-------------------------------------------
+
+	/**
+	 *
+	 * @param key the Region key
+	 * @param pdxInstance the region value PDX
+	 * @param <Key> the key type
+	 * @return the wrapper object
+	 */
+	public static <Key extends Serializable> SerializationPdxEntryWrapper toSerializePdxEntryWrapper(Key key, String valueClassName, PdxInstance pdxInstance)
+	{
+		return new SerializationPdxEntryWrapper(key,valueClassName,pdxInstance);
+	}
+
+	public static PdxInstance fromObject(Object obj)
+	{
+		if(obj instanceof PdxInstance)
+			return (PdxInstance)obj;
+
+
+		try
+		{
+
+			String json = toJsonFromNondPdxObject(obj);
+
+			return JSONFormatter.fromJSON(json);
+
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException(e);
+		}
+
+	}//-------------------------------------------
+
+
+}
---
+++ src/test/java/io/pivotal/services/dataTx/geode/serialization/SerializationPdxEntryWrapperTest.java
@@ -0,0 +1,71 @@
+package io.pivotal.services.dataTx.geode.serialization;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.pivotal.services.dataTx.geode.demo.SimpleObject;
+import nyla.solutions.core.security.user.data.UserProfile;
+import org.junit.Test;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.*;
+
+public class SerializationPdxEntryWrapperTest
+{
+
+    @Test
+    public void isCustom()
+    {
+        assertTrue(SerializationPdxEntryWrapper.isCustom(new SimpleObject()));
+        assertFalse(SerializationPdxEntryWrapper.isCustom(null));
+        assertFalse(SerializationPdxEntryWrapper.isCustom(LocalDateTime.now()));
+    }
+
+    @Test
+    public void test_setJson_not_allow_json_without_type()
+            throws Exception
+    {
+        SerializationPdxEntryWrapper<Serializable> s = new SerializationPdxEntryWrapper<>();
+
+        try{
+            s.setValueJson("");
+        }
+        catch(IllegalArgumentException e)
+        {
+            fail();
+        }
+
+        try{
+            s.setValueJson("Sdfdsfsf sdfsfdsf");
+            fail();
+        }
+        catch(IllegalArgumentException e)
+        {
+
+        }
+
+
+        try{
+            UserProfile expected = new UserProfile();
+            expected.setEmail("test");
+
+            s.setValueJson(new ObjectMapper().writeValueAsString(expected));
+            fail();
+        }
+        catch(IllegalArgumentException e)
+        {
+
+        }
+
+        try{
+            s.setValueJson("@type");
+            fail();
+        }
+        catch(IllegalArgumentException e)
+        {
+        }
+
+        s.setValueJson("{\"@type\":\"java.util.Calendar\"}");
+
+    }
+}
